79d6d2d80d6d9ea84b40be2d367b527cc9496447
Zorgatone <zorgatone@gmail.com>
2018-06-16 11:35:27 +0200

    more tests

--- src/enumValues.test.js
+++ src/enumValues.test.js
@@ -9,25 +9,34 @@ describe('EnumValues', function () {
             NumericValuesTestEnum[NumericValuesTestEnum["A"] = 0] = "A";
             NumericValuesTestEnum[NumericValuesTestEnum["B"] = 1] = "B";
             NumericValuesTestEnum[NumericValuesTestEnum["C"] = 2] = "C";
+            NumericValuesTestEnum[NumericValuesTestEnum["c"] = 2] = "c";
+            NumericValuesTestEnum[NumericValuesTestEnum["D"] = 3] = "D";
+            NumericValuesTestEnum[NumericValuesTestEnum["K"] = 10] = "K";
         })(NumericValuesTestEnum || (NumericValuesTestEnum = {}));
         it('getNames should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getNames(NumericValuesTestEnum)).to.deep.equal(['A', 'B', 'C']);
+            chai_1.expect(enumValues_1.EnumValues.getNames(NumericValuesTestEnum)).to.deep.equal(['A', 'B', 'C', 'c', 'D', 'K']);
         });
         it('getValues should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getValues(NumericValuesTestEnum)).to.deep.equal([0, 1, 2]);
+            chai_1.expect(enumValues_1.EnumValues.getValues(NumericValuesTestEnum)).to.deep.equal([0, 1, 2, 2, 3, 10]);
         });
         it('getNameFromValue should return the name for that value', function () {
             var expectedResult = "B";
             chai_1.expect(enumValues_1.EnumValues.getNameFromValue(NumericValuesTestEnum, 1)).equal(expectedResult);
         });
-        it('getNameFromValue should return null', function () {
+        it('getNameFromValue should return null with duplicate value', function () {
+            chai_1.expect(enumValues_1.EnumValues.getNameFromValue(NumericValuesTestEnum, 2)).to.be.null;
+        });
+        it('getNameFromValue should return null when not found', function () {
             chai_1.expect(enumValues_1.EnumValues.getNameFromValue(NumericValuesTestEnum, 11)).to.be.null;
         });
         it('getNamesAndValues should return correct values', function () {
             var expectedResult = [
                 { name: 'A', value: 0 },
                 { name: 'B', value: 1 },
-                { name: 'C', value: 2 }
+                { name: 'C', value: 2 },
+                { name: 'c', value: 2 },
+                { name: 'D', value: 3 },
+                { name: 'K', value: 10 }
             ];
             chai_1.expect(enumValues_1.EnumValues.getNamesAndValues(NumericValuesTestEnum)).to.deep.equal(expectedResult);
         });
@@ -38,25 +47,30 @@ describe('EnumValues', function () {
             StringValuesTestEnum["A"] = "AValue";
             StringValuesTestEnum["B"] = "BValue";
             StringValuesTestEnum["C"] = "CValue";
+            StringValuesTestEnum["c"] = "CValue";
         })(StringValuesTestEnum || (StringValuesTestEnum = {}));
         it('getNames should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getNames(StringValuesTestEnum)).to.deep.equal(['A', 'B', 'C']);
+            chai_1.expect(enumValues_1.EnumValues.getNames(StringValuesTestEnum)).to.deep.equal(['A', 'B', 'C', 'c']);
         });
         it('getNameFromValue should return the name for that value', function () {
             var expectedResult = "B";
             chai_1.expect(enumValues_1.EnumValues.getNameFromValue(StringValuesTestEnum, 'BValue')).equal(expectedResult);
         });
-        it('getNameFromValue should return null', function () {
+        it('getNameFromValue should return null with duplicate value', function () {
+            chai_1.expect(enumValues_1.EnumValues.getNameFromValue(StringValuesTestEnum, 'CValue')).to.be.null;
+        });
+        it('getNameFromValue should return null when not found', function () {
             chai_1.expect(enumValues_1.EnumValues.getNameFromValue(StringValuesTestEnum, 'BadValue')).to.be.null;
         });
         it('getValues should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getValues(StringValuesTestEnum)).to.deep.equal(['AValue', 'BValue', 'CValue']);
+            chai_1.expect(enumValues_1.EnumValues.getValues(StringValuesTestEnum)).to.deep.equal(['AValue', 'BValue', 'CValue', 'CValue']);
         });
         it('getNamesAndValues should return correct values', function () {
             var expectedResult = [
                 { name: 'A', value: 'AValue' },
                 { name: 'B', value: 'BValue' },
-                { name: 'C', value: 'CValue' }
+                { name: 'C', value: 'CValue' },
+                { name: 'c', value: 'CValue' }
             ];
             chai_1.expect(enumValues_1.EnumValues.getNamesAndValues(StringValuesTestEnum)).to.deep.equal(expectedResult);
         });
@@ -67,18 +81,20 @@ describe('EnumValues', function () {
             MixedEnum["A"] = "first";
             MixedEnum[MixedEnum["B"] = 2] = "B";
             MixedEnum["c"] = "third";
+            MixedEnum[MixedEnum["@"] = 2] = "@";
         })(MixedEnum || (MixedEnum = {}));
         it('getNames should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getNames(MixedEnum)).to.deep.equal(['A', 'B', 'c']);
+            chai_1.expect(enumValues_1.EnumValues.getNames(MixedEnum)).to.deep.equal(['A', 'B', 'c', '@']);
         });
         it('getValues should return correct values', function () {
-            chai_1.expect(enumValues_1.EnumValues.getValues(MixedEnum)).to.deep.equal(['first', 2, 'third']);
+            chai_1.expect(enumValues_1.EnumValues.getValues(MixedEnum)).to.deep.equal(['first', 2, 'third', 2]);
         });
         it('getNamesAndValues should return correct values', function () {
             var expectedResult = [
                 { name: 'A', value: 'first' },
                 { name: 'B', value: 2 },
-                { name: 'c', value: 'third' }
+                { name: 'c', value: 'third' },
+                { name: '@', value: 2 }
             ];
             chai_1.expect(enumValues_1.EnumValues.getNamesAndValues(MixedEnum)).to.deep.equal(expectedResult);
         });
--- src/enumValues.test.ts
+++ src/enumValues.test.ts
@@ -3,21 +3,24 @@ import { EnumValues } from "./enumValues";
 import { expect } from 'chai';
 
 
-
 describe('EnumValues', () => {
   describe('numeric values', () => {
     enum NumericValuesTestEnum {
       A,
       B,
-      C
+      C,
+      c = 2, // note lower c has same value of upper C
+      D,
+      K = 10,
     }
 
     it('getNames should return correct values', () => {
-      expect(EnumValues.getNames(NumericValuesTestEnum)).to.deep.equal(['A', 'B', 'C']);
+      expect(EnumValues.getNames(NumericValuesTestEnum)).to.deep.equal(['A', 'B', 'C', 'c', 'D', 'K']);
     });
 
     it('getValues should return correct values', () => {
-      expect(EnumValues.getValues<number>(NumericValuesTestEnum)).to.deep.equal([0, 1, 2]);
+      // Note there are two identical '10' numbers in the array
+      expect(EnumValues.getValues<number>(NumericValuesTestEnum)).to.deep.equal([0, 1, 2, 2, 3, 10]);
     });
 
     it('getNameFromValue should return the name for that value', () => {
@@ -26,7 +29,11 @@ describe('EnumValues', () => {
       expect(EnumValues.getNameFromValue(NumericValuesTestEnum, 1)).equal(expectedResult);
     });
 
-    it('getNameFromValue should return null', () => {
+    it('getNameFromValue should return null with duplicate value', () => {
+      expect(EnumValues.getNameFromValue(NumericValuesTestEnum, 2)).to.be.null;
+    });
+
+    it('getNameFromValue should return null when not found', () => {
       expect(EnumValues.getNameFromValue(NumericValuesTestEnum, 11)).to.be.null;
     });
 
@@ -34,7 +41,10 @@ describe('EnumValues', () => {
       var expectedResult = [
         { name: 'A', value: 0 },
         { name: 'B', value: 1 },
-        { name: 'C', value: 2 }
+        { name: 'C', value: 2 },
+        { name: 'c', value: 2 },
+        { name: 'D', value: 3 },
+        { name: 'K', value: 10 }
       ];
 
       expect(EnumValues.getNamesAndValues<number>(NumericValuesTestEnum)).to.deep.equal(expectedResult);
@@ -45,11 +55,12 @@ describe('EnumValues', () => {
     enum StringValuesTestEnum {
       A = 'AValue',
       B = 'BValue',
-      C = 'CValue'
+      C = 'CValue',
+      c = 'CValue' // note lower c has same value of upper C
     }
 
     it('getNames should return correct values', () => {
-      expect(EnumValues.getNames(StringValuesTestEnum)).to.deep.equal(['A', 'B', 'C']);
+      expect(EnumValues.getNames(StringValuesTestEnum)).to.deep.equal(['A', 'B', 'C', 'c']);
     });
 
     it('getNameFromValue should return the name for that value', () => {
@@ -58,19 +69,24 @@ describe('EnumValues', () => {
       expect(EnumValues.getNameFromValue(StringValuesTestEnum, 'BValue')).equal(expectedResult);
     });
 
-    it('getNameFromValue should return null', () => {
+    it('getNameFromValue should return null with duplicate value', () => {
+      expect(EnumValues.getNameFromValue(StringValuesTestEnum, 'CValue')).to.be.null;
+    });
+
+    it('getNameFromValue should return null when not found', () => {
       expect(EnumValues.getNameFromValue(StringValuesTestEnum, 'BadValue')).to.be.null;
     });
 
     it('getValues should return correct values', () => {
-      expect(EnumValues.getValues<string>(StringValuesTestEnum)).to.deep.equal(['AValue', 'BValue', 'CValue']);
+      expect(EnumValues.getValues<string>(StringValuesTestEnum)).to.deep.equal(['AValue', 'BValue', 'CValue', 'CValue']);
     });
 
     it('getNamesAndValues should return correct values', () => {
       var expectedResult = [
         { name: 'A', value: 'AValue' },
         { name: 'B', value: 'BValue' },
-        { name: 'C', value: 'CValue' }
+        { name: 'C', value: 'CValue' },
+        { name: 'c', value: 'CValue' }
       ];
 
       expect(EnumValues.getNamesAndValues<string>(StringValuesTestEnum)).to.deep.equal(expectedResult);
@@ -81,22 +97,24 @@ describe('EnumValues', () => {
     enum MixedEnum {
       A = 'first',
       B = 2,
-      c = 'third'
+      c = 'third',
+      '@' = 2,
     }
 
     it('getNames should return correct values', () => {
-      expect(EnumValues.getNames(MixedEnum)).to.deep.equal(['A', 'B', 'c']);
+      expect(EnumValues.getNames(MixedEnum)).to.deep.equal(['A', 'B', 'c', '@']);
     });
 
     it('getValues should return correct values', () => {
-      expect(EnumValues.getValues(MixedEnum)).to.deep.equal(['first', 2, 'third']);
+      expect(EnumValues.getValues(MixedEnum)).to.deep.equal(['first', 2, 'third', 2]);
     });
 
     it('getNamesAndValues should return correct values', () => {
       const expectedResult = [
         { name: 'A', value: 'first' },
         { name: 'B', value: 2 },
-        { name: 'c', value: 'third' }
+        { name: 'c', value: 'third' },
+        { name: '@', value: 2 }
       ];
 
       expect(EnumValues.getNamesAndValues(MixedEnum)).to.deep.equal(expectedResult);
